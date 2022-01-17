3f98ea9db874d801951eb8c751a90f12ef1763a6
Chuck <cew182@gmail.com>
2022-01-17 16:15:07 +0100

    Starship Quadrants

---
+++ Starship Quadrants.js
@@ -0,0 +1,29 @@
+/* Updated for v9 by Sputt*/
+(async function(){
+if (!token) {
+  ui.notifications.info("Seleziona un'astronave per visualizzare gli archi di fuoco");
+  return
+}
+const rotation = (token.data.flags["about-face"] && (typeof token.data.flags["about-face"].direction) !== "undefined") ? token.data.flags["about-face"].direction : token.data.rotation;
+const type = "MeasuredTemplate";
+const time = 2500;
+const headings = [60, 120, 240, 300];
+const specs = headings.map(h => {
+    let borderColor = "#45afeb"
+    if( h < 180 ) borderColor = "#f49045";
+    return {t: "ray",
+      user: game.user._id,
+      x: token.x + token.w / 2,
+      y: token.y + token.h / 2,
+      direction: rotation+ h,
+      distance: 20,
+      width: 0.001,
+      borderColor,
+      fillColor: "#000000"
+    }
+});
+const scene = game.scenes.current;
+const ents = await scene.createEmbeddedDocuments(type, specs);
+const deletions = ents.map(i => i.id);
+setTimeout(() => {scene.deleteEmbeddedDocuments(type, deletions)}, time);
+})()
