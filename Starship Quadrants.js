9114878c41bcb41f544a015a3081fea80d2fb050
Iankid <60759323+Iankid@users.noreply.github.com>
2022-02-04 10:02:31 -0800

    Updated heading and border color settings

--- Starship Quadrants.js
+++ Starship Quadrants.js
@@ -7,10 +7,10 @@ if (!token) {
 const rotation = (token.data.flags["about-face"] && (typeof token.data.flags["about-face"].direction) !== "undefined") ? token.data.flags["about-face"].direction : token.data.rotation;
 const type = "MeasuredTemplate";
 const time = 2500;
-const headings = [60, 120, 240, 300];
+const headings = [30, 150, 210, -30];
 const specs = headings.map(h => {
     let borderColor = "#45afeb"
-    if( h < 180 ) borderColor = "#f49045";
+    if( h < 90 ) borderColor = "#00ff00";
     return {t: "ray",
       user: game.user.id,
       x: token.x + token.w / 2,
